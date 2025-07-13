c1b2d8017c6601ca0e8baa87c8ffad366af4a0ea
anirudh-248 <anirudhphaniraj1@gmail.com>
2025-07-13 12:27:24 +0530

    Fix #2: removed dangerous default argument

--- app/utils/success_handler.py
+++ app/utils/success_handler.py
@@ -1,11 +1,13 @@
 from typing import Any, Optional
 
-def success_response(message: str, data: Optional[Any] = {}) -> dict:
+def success_response(message: str, data: Optional[Any] = None) -> dict:
     """
     Generate a success response dictionary.
+
     Parameters:
         - message (str): The success message to include in the response.
         - data (Optional[Any]): Optional additional data to include in the response. Can be of any type.
+
     Returns:
         - dict: A dictionary containing the success status, message, and optionally the data if provided.
     """
